6afc0774e2883074bbf05fb4603ee9590c3fd55d
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-05-29 15:00:04 -0500

    add categoria repository

---
+++ repository/categoria.go
@@ -0,0 +1,18 @@
+package repository
+
+import (
+	"github.com/marcos-nsantos/alura-flix/models"
+	"gorm.io/gorm"
+)
+
+type CategoriaRepository struct {
+	db *gorm.DB
+}
+
+func NewCategoriaRepository(db *gorm.DB) *CategoriaRepository {
+	return &CategoriaRepository{db}
+}
+
+func (cr CategoriaRepository) Create(categoria *models.Categoria) error {
+	return cr.db.Create(categoria).Error
+}
