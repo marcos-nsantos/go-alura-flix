7f4d69a14cd6da600e42d2e2934db2c4d9a34d57
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-05-29 15:18:36 -0500

    add controller to show all categorias

---
+++ controllers/categorias/showAllCategorias.go
@@ -0,0 +1,25 @@
+package categoriaControllers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/marcos-nsantos/alura-flix/database"
+	"github.com/marcos-nsantos/alura-flix/repository"
+	"net/http"
+)
+
+func ShowAllCategorias(c *gin.Context) {
+	db, err := database.Connect()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	categoriaRepository := repository.NewCategoriaRepository(db)
+	categorias, err := categoriaRepository.FindAll()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, categorias)
+}
