186599eb71586af025c915d29d08763e70faeeea
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-05-29 15:18:07 -0500

    add function to retrieve all categorias from database

--- repository/categoria.go
+++ repository/categoria.go
@@ -16,3 +16,9 @@ func NewCategoriaRepository(db *gorm.DB) *CategoriaRepository {
 func (cr CategoriaRepository) CreateCategoria(categoria *models.Categoria) error {
 	return cr.db.Create(categoria).Error
 }
+
+func (cr CategoriaRepository) FindAll() ([]*models.Categoria, error) {
+	var categorias []*models.Categoria
+	err := cr.db.Find(&categorias).Error
+	return categorias, err
+}
