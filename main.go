13b88c67d0bb895f12c0494c85da6373c0ca730c
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-05-29 10:54:43 -0500

    register custom validators

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"github.com/marcos-nsantos/alura-flix/database"
 	"github.com/marcos-nsantos/alura-flix/routes"
+	"github.com/marcos-nsantos/alura-flix/utils"
 )
 
 func init() {
 	database.Migrate()
+	utils.RegisterValidators()
 }
 
 func main() {
