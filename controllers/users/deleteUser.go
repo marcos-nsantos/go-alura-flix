8798d225d16778f9403e724367cc2062f382b56c
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-06-05 13:47:28 -0500

    add controller to delete user

---
+++ controllers/users/deleteUser.go
@@ -0,0 +1,42 @@
+package usersController
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/marcos-nsantos/alura-flix/database"
+	"github.com/marcos-nsantos/alura-flix/models"
+	"github.com/marcos-nsantos/alura-flix/repository"
+	"net/http"
+	"strconv"
+)
+
+func DeleteUser(c *gin.Context) {
+	userID := c.Param("id")
+	userIDUint, err := strconv.ParseUint(userID, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+
+	db, err := database.Connect()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	var user models.User
+	user.ID = uint(userIDUint)
+
+	userRepository := repository.NewUserRepository(db)
+	if _, err := userRepository.FindUserByID(user.ID); err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+
+	err = userRepository.DeleteUser(&user)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"success": "User deleted successfully"})
+}
