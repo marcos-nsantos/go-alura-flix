100fc890754ea552c6a558c9c8a8eb03d3e23554
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-06-05 11:31:22 -0500

    create function to hash password

--- models/user.go
+++ models/user.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
@@ -9,4 +10,12 @@ type User struct {
 	Name     string `json:"name" binding:"required,notblank"`
 	Email    string `json:"email,notblank"`
 	Password string `json:"password,notblank,omitempty"`
+}
+
+func (u *User) HashPassword() (string, error) {
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(passwordHash), nil
 }
